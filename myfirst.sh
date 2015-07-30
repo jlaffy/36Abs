c9c1c9f7d012237089861ad61d0b832d91ef5aff
Julie Laffy <julielaffy@apomac.local>
2015-07-30 17:57:00 +0100

    updated remove0s.sh, updated backupcontdiffs.sh, have seqid.csv files for all pairs containing the residue position and the residue id and the difference in contact probability between small and large antigen volume.

--- myfirst.sh
+++ myfirst.sh
@@ -2,9 +2,9 @@
 #Edit R script's sequence ids to produce graphs for all H-L chain pairings, including var domain key
 #julielaffy 26/7/15
 
-date= date +%F
+date=date +%F
 
-#array= ( immh103MV2 immh149MV5 immh159MU5 naih111 preh118 preh119 preh138MV4 preh146 preh149MU5 preh149MU7 )
+#array=( immh103MV2 immh149MV5 immh159MU5 naih111 preh118 preh119 preh138MV4 preh146 preh149MU5 preh149MU7 )
 
 seqid= $1
 
