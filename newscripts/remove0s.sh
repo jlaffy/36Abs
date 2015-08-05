04885132898da5c6bbbdd6ce5a2b9285114a6842
Julie Laffy <julielaffy@apomac.local>
2015-08-05 20:42:57 +0100

    same for all heavy chains as last did for p43h149MU5 only. new folder N120-IMML103-NAIL107 for 12 csvs with difs between 3 light chain pairings. use change_Seqid.sh to loop through R scripts with heavy chain ids and produce csvs etc. 1H3L (1 heavy 3 light) is R script run. Need to organise scripts, prob one of most important things to do at the moment.

--- newscripts/remove0s.sh
+++ newscripts/remove0s.sh
@@ -3,17 +3,14 @@
 Harray=( immh103MV2 immh149MV5 immh159MU5 naih111 preh118 preh119 preh138MV4 preh146 preh149MU5 preh149MU7 )
 Harray2=( preh149MU5 )
 Larray=( IMML103 NAIL107 )
-Larray2=(  )
 
-#for j in "${Larray2[@]}"
-#do
-for i in "${Harray2[@]}"
+for i in "${Harray[@]}"
 do
-	#DIR=( ~/36Abs/results/kappa_pairs/proABC/${j}/${i}/${i} )
-	DIR=( ~/36Abs/results/nail120-nail107/${i}_120107.csv )
-	echo $j $i
-	sed 's/"//g' ${DIR} > ${DIR}a && mv ${DIR}a ${DIR}
-	sed 's/^,/preh149MU5_120107,/' ${DIR} > ${DIR}a && mv ${DIR}a ${DIR}
-	egrep -v '[0-9]{1,3}...0$' ${DIR} > ${DIR}b
+	DIR=( ~/36Abs/results/$1/${i}_$1.csv )
+	echo $i $1 
+	#sed 's/"//g' ${DIR} > ${DIR}a && mv ${DIR}a ${DIR}
+	#sed "s/^,/${i}_$1,/" ${DIR} > ${DIR}a && mv ${DIR}a ${DIR}
+	sed "s/${i}_$1/${i}/" ${DIR} > ${DIR}a && mv ${DIR}a ${DIR}
+	sed -E "s/[a-z0-9]{6,9}_dif/$1/" ${DIR} > ${DIR}a && mv ${DIR}a ${DIR}
+	#egrep -v '[0-9]{1,3}...0$' ${DIR} > ${DIR}a
 done
-#done
--- results/imml103-nail107/nail107-imml103contdiffs.R
+++ results/imml103-nail107/nail107-imml103contdiffs.R
@@ -1,10 +1,10 @@
 #!/usr/bin/Rscript
-preh149MU5kap2 <- read.csv("/Users/julielaffy/36Abs/results/kappa_pairs/proABC/NAIL107/preh149MU5/preh149MU5big/Prob_Heavy.csv")
-preh149MU5kap <- read.csv("/Users/julielaffy/36Abs/results/kappa_pairs/proABC/IMML103/preh149MU5/preh149MU5big/Prob_Heavy.csv")
+preh149MU7kap2 <- read.csv("/Users/julielaffy/36Abs/results/kappa_pairs/proABC/NAIL107/preh149MU7/preh149MU7big/Prob_Heavy.csv")
+preh149MU7kap <- read.csv("/Users/julielaffy/36Abs/results/kappa_pairs/proABC/IMML103/preh149MU7/preh149MU7big/Prob_Heavy.csv")
 
 #select rows from data.frame
-cont_all_kap2 <- preh149MU5kap2[2,]
-cont_all_kap <- preh149MU5kap[2,]
+cont_all_kap2 <- preh149MU7kap2[2,]
+cont_all_kap <- preh149MU7kap[2,]
 
 #exclude first element
 cont_all_kap2 <- c(t(cont_all_kap2[-1]))
@@ -17,16 +17,16 @@ plot(sequence_nums, cont_all_kap2, type="h", col="purple", lwd=6, xlab = "a.a po
 lines(sequence_nums, cont_all_kap, type="h", col="orange", lwd=4)
 axis(side=1, at=seq(1, length(cont_all_kap2), by=1))
 
-legend('topright', c("small antigen vol","large antigen vol"), fill=c("purple","orange"), bty="n", cex = 0.85, title = "preh149MU5")
+legend('topright', c("small antigen vol","large antigen vol"), fill=c("purple","orange"), bty="n", cex = 0.85, title = "preh149MU7")
 legend('topleft', c("1-30 = FR1","31-35 = CDR1","36-49 = FR2","50-68 = CDR2","69-100 = FR3", "101-121 = CDR3", "122-132 = FR4"), bty="n", cex = 0.85)
 
 #select amino acid seq from data.frame, exclude first element, number a.as in a dataframe
-a.a.seq <- preh149MU5kap2[1,]
+a.a.seq <- preh149MU7kap2[1,]
 a.a.seq <- c(t(a.a.seq[-1]))
 a.a.seq = data.frame(a.a.seq)
 
 kap2kapim_dif <- as.numeric(cont_all_kap2)-as.numeric(cont_all_kap)
 as.factor(kap2kapim_dif)
 df <- data.frame(a.a.seq, kap2kapim_dif)
-write.csv(df, file = "~/36Abs/results/imml103-nail107/preh149MU5_120103.csv")
+write.csv(df, file = "~/36Abs/results/IMML103-NAIL107/preh149MU7_IMML103-NAIL107.csv")
 
