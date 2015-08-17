08a73fb297d19ae9ca50a0e3b59ec2b7eeb3399e
Julie Laffy <julielaffy@apomac.local>
2015-08-17 17:14:19 +0100

    working on proABC antigen volume not known data.

--- scripts/proABC_batch_clean_no_screens.py
+++ scripts/proABC_batch_clean_no_screens.py
@@ -1,9 +1,12 @@
+# !usr/bin/python
+
 import datetime
 import traceback
 import sys
 import re
 import json
 import csv
+import os, sys
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -11,81 +14,95 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
 from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
 
-#exclude project_names[i] "immh103MU2". hvy_chain ""EVQLVESGGGLVKPGGSLRLSCAASGFTFSNAWMSWVRQAPGKGLEWVGRIKSKTDGGTTDYAAPVKGRFTISRDDSKNTLYLQMNSLKTEDTAVYYCTTAPINIVVVVPAAIKDRDYYYYGMDVWGQGTTVTVSS","
-light_chain = "EIVLTQSPATLSLSPGERATLSCRASQSVSSSYLAWYQQKPGQAPRLLIYGASSRATGIPDRFSGSGSGTDFTLTISRLEPEDFAVYYCQQYGSSPYTFGQGTKLEIK"
-project_names = ["immh103MV2", "immh149MV5", "immh159MU5", "naih111", "preh118", "preh119", "preh138MV4", "preh146","preh149MU5", "preh149MU7", "preh159MV6"]
-hvy_chains = ["EVQLVESGGGLVKPGGSLRLSCAASGFTFSNAWMSWVRQAPGKGLEWVGRIKSKTDGGTTDYAAPVKGRFTISRDDSKNTLYLQMNSLKTEDTAVYYCTTVRDIVVVPAAIGLNYYYGMDVWGQGTTVTVSS", 
-	"QVQLVESGGGVVQPGRSLRLSCAASGFTFSNAWMSWVRQAPGKGLEWVAVISYDGSNKYYADSVKGRFTISRDNSKNTLYLQMNSLRAEDTAVYYCAKDQFVAVVVPAAIHLGYYYGMDVWGQGTTVTVSS", 
-	"EVQLVESGGGLVKPGGSLRLSCAASGFMFSSYEMNWVRQAPGKGLEWVGRIKSKTDGGTTDYAAPVKGRFTISRDDSKNTLYLQMNSLKTEDTAVYYCTTDLFLRARVVPAAMAPYYYGMDVWGQGTTVTVSS", 
-	"QVQLVESGGGVVQPGRSLRLSCAASGFTFSSYGMHWVRQAPGKGLEWVAVISYDGSNKYYADSVKGRFTISRDNSKNTLYLQMNSLRAEDTAVYYCAKTLLDVVVVPAATPQNYYYYGMDVWGQGTTVTVSS", 
-	"QVQLVESGGGVVQPGRSLRLSCAASGFTFSTYPMHWVRQAPGKGLEWVAFLWHDGTEEFYADSVRGRFTISRDNSKNTLYLQMDGLRADDTALYYCARDTGVGGLVVPAATGDLAGYYYGMDVWGQGTTVTVSS", 
-	"EVQLVESGGGLVKPGGSLRLSCAASGFTFSNAWMSWVRQAPGKGLEWVGRIKSKTDGGTTDYAAPVKGRFTISRDDSKNTLYLQMNSLKTEDTAVYYCTTHIVVVPAAIPSHYYYYYGMDVWGQGTTVTVSS", 
-	"EVQLVESGGGLVKPGGSLRLSCAASGFMFSSYEMNWVRQAPGKGLEWVGRIKSKTDGGTTDYAAPVKGRFTISRDDSKNTLYLQMNSLKTEDTAVYYCTTDLFLRARVVPAAMAPYYYGMDVWGQGTTVTVSS", 
-	"QVQLVESGGGVVQPGRSLRLSCAASGFTFSSYAMSWVRQAPGKGLEWVAVISYDGSNKYYADSVKGRFTISRDNSKNTLYLQMNSLRAEDTAVYYCAKTASFVVVPAAISDGDGYYYYMDVWGKGTTVTVSS", 
-	"QVQLVESGGGVVQPGRSLRLSCAASGFTFSSYGMHWVRQAPGKGLEWVAVISYDGSNKYYADSVKGRFTISRDNSKNTLYLQMNSLRAEDTAVYYCAKGAPNIVVVPAAKSAHYYYGMDVWGQGTTVTVSS", 
-	"EVQLVESGGGLVKPGGSLRLSCAASGFTFSNAWMSWVRQAPGKGLEWVGRIKSKTDGGTTDYAAPVKGRFTISRDDSKNTLYLQMNSLKTEDTAVYYCTTDRWGIVVVPAAKLVSYYGMDVWGQGTTVTVSS", 
-	"EVQLVESGGGLVKPGGSLRLSCAASGFTFSNAWMSWVRQAPGKGLEWVGRIKSKTDGGTTDYAAPVKGRFTISRDDSKNTLYLQMNSLKTEDTAVYYCILVHIVVVPAAINGYYDFWSGWDVWGQGTTVTVSS"]
+#exclude light_project names NAIL107 "EIVLTQSPATLSLSPGERATLSCRASQSVSSSYLAWYQQKPGQAPRLLIYGASSRATGIPDRFSGSGSGTDFTLTISRLEPEDFAVYYCQQYGSSPYTFGQGTKLEIK"
 
-volume = ["small", "big"]
+#exclude hvy_project_names[i] "immh103MU2". hvy_chain ""EVQLVESGGGLVKPGGSLRLSCAASGFTFSNAWMSWVRQAPGKGLEWVGRIKSKTDGGTTDYAAPVKGRFTISRDDSKNTLYLQMNSLKTEDTAVYYCTTAPINIVVVVPAAIKDRDYYYYGMDVWGQGTTVTVSS","
+light_chains = ["QSVLTQPPSASGTPGQRVTISCSGSSSNIGSNTVNWYQQLPGTAPKLLIYSNNQRPSGVPDRFSGSKSGTSASLAISGLQSEDEADYYCAAWDDSLNGVVFGGGTKLTVL",
+                "QSVLTQPPSASGTPGQRVTISCSGSSSNIGSNTVNWYQQLPGTAPKLLIYSNNQRPSGVPDRFSGSKSGTSASLAISGLQSEDEADYYCAAWDDSLNGVVFGGGTKLTVL"]
+hvy_project_names = ["immh103MV2", "immh149MV5", "immh159MU5", "naih111", "preh118", "preh119", "preh138MV4", "preh146","preh149MU5", "preh149MU7", "preh159MV6"]
+light_project_names = ["NAIL120", "IMML103"]
+hvy_chains = ["EVQLVESGGGLVKPGGSLRLSCAASGFTFSNAWMSWVRQAPGKGLEWVGRIKSKTDGGTTDYAAPVKGRFTISRDDSKNTLYLQMNSLKTEDTAVYYCTTVRDIVVVPAAIGLNYYYGMDVWGQGTTVTVSS",
+    "QVQLVESGGGVVQPGRSLRLSCAASGFTFSNAWMSWVRQAPGKGLEWVAVISYDGSNKYYADSVKGRFTISRDNSKNTLYLQMNSLRAEDTAVYYCAKDQFVAVVVPAAIHLGYYYGMDVWGQGTTVTVSS",
+    "EVQLVESGGGLVKPGGSLRLSCAASGFMFSSYEMNWVRQAPGKGLEWVGRIKSKTDGGTTDYAAPVKGRFTISRDDSKNTLYLQMNSLKTEDTAVYYCTTDLFLRARVVPAAMAPYYYGMDVWGQGTTVTVSS",
+    "QVQLVESGGGVVQPGRSLRLSCAASGFTFSSYGMHWVRQAPGKGLEWVAVISYDGSNKYYADSVKGRFTISRDNSKNTLYLQMNSLRAEDTAVYYCAKTLLDVVVVPAATPQNYYYYGMDVWGQGTTVTVSS",
+    "QVQLVESGGGVVQPGRSLRLSCAASGFTFSTYPMHWVRQAPGKGLEWVAFLWHDGTEEFYADSVRGRFTISRDNSKNTLYLQMDGLRADDTALYYCARDTGVGGLVVPAATGDLAGYYYGMDVWGQGTTVTVSS",
+    "EVQLVESGGGLVKPGGSLRLSCAASGFTFSNAWMSWVRQAPGKGLEWVGRIKSKTDGGTTDYAAPVKGRFTISRDDSKNTLYLQMNSLKTEDTAVYYCTTHIVVVPAAIPSHYYYYYGMDVWGQGTTVTVSS",
+    "EVQLVESGGGLVKPGGSLRLSCAASGFMFSSYEMNWVRQAPGKGLEWVGRIKSKTDGGTTDYAAPVKGRFTISRDDSKNTLYLQMNSLKTEDTAVYYCTTDLFLRARVVPAAMAPYYYGMDVWGQGTTVTVSS",
+    "QVQLVESGGGVVQPGRSLRLSCAASGFTFSSYAMSWVRQAPGKGLEWVAVISYDGSNKYYADSVKGRFTISRDNSKNTLYLQMNSLRAEDTAVYYCAKTASFVVVPAAISDGDGYYYYMDVWGKGTTVTVSS",
+    "QVQLVESGGGVVQPGRSLRLSCAASGFTFSSYGMHWVRQAPGKGLEWVAVISYDGSNKYYADSVKGRFTISRDNSKNTLYLQMNSLRAEDTAVYYCAKGAPNIVVVPAAKSAHYYYGMDVWGQGTTVTVSS",
+    "EVQLVESGGGLVKPGGSLRLSCAASGFTFSNAWMSWVRQAPGKGLEWVGRIKSKTDGGTTDYAAPVKGRFTISRDDSKNTLYLQMNSLKTEDTAVYYCTTDRWGIVVVPAAKLVSYYGMDVWGQGTTVTVSS",
+    "EVQLVESGGGLVKPGGSLRLSCAASGFTFSNAWMSWVRQAPGKGLEWVGRIKSKTDGGTTDYAAPVKGRFTISRDDSKNTLYLQMNSLKTEDTAVYYCILVHIVVVPAAINGYYDFWSGWDVWGQGTTVTVSS"]
 
 
-for i in range(len(hvy_chains)):
+#volume = ["small", "big"]
 
-	for vol in volume:
 
-		try: 
+for j in range(len(light_chains)):
 
-			print "Starting " + str(i)
+    #for vol in volume:
+    for i in range(len(hvy_chains)):
 
-			results_file = "~/OneDrive/36Abs/results/kappa_pair/proABC/nail107/"+project_names[i]+vol+"/"
+        try:
 
-			fp = webdriver.FirefoxProfile()
-			fp.set_preference("browser.download.folderList",2)
-			fp.set_preference("browser.download.manager.showWhenStarting",False)
-			fp.set_preference("browser.download.dir", results_file)
-			fp.set_preference("browser.helperApps.neverAsk.saveToDisk","text/csv")
+            print "Starting " + str(i)
 
-			driver = webdriver.Firefox(firefox_profile=fp)
-			driver.set_window_size(1024, 768)
-			driver.implicitly_wait(40) 
+            oldheavy = "Prob_Heavy.csv"
+            oldlight = "Prob_Light.csv"
+            newheavy =  light_project_names[j]+"_"+hvy_project_names[i]+"_Prob_Heavy.csv"
+            newlight =  light_project_names[j]+"_"+hvy_project_names[i]+"_Prob_Light.csv"
+            path = "~/36Abs/results/rawdat_proabc/"+light_project_names[i]+"/"
+            print path
+            results_file = "~/36Abs/results/rawdat_proabc/"+light_project_names[j]+"/"
 
-			#get page
-			driver.get("http://circe.med.uniroma1.it/proABC/")
-			element = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "invio")))
-			print "Got page..."
+            fp = webdriver.FirefoxProfile()
+            fp.set_preference("browser.download.folderList",2)
+            fp.set_preference("browser.download.manager.showWhenStarting",False)
+            fp.set_preference("browser.download.dir", results_file)
+            fp.set_preference("browser.helperApps.neverAsk.saveToDisk","text/csv")
 
-			#fill form
-			driver.find_element_by_name("name").send_keys(project_names[i])
-			driver.find_element_by_name("email").send_keys("jlaffy7@gmail.com")
-			driver.find_element_by_id("light").send_keys(light_chain)
-			driver.find_element_by_id("heavy").send_keys(hvy_chains[i])
-			driver.find_element_by_xpath("//select[@name='volume']/option[@value='"+vol+"']").click()
-			driver.find_element_by_id("invio").click()
-			print "Filled form..."
-			
-			#goes to the Plot tab as default	
-			element = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.ID, "IDgoogleTable")))
+            driver = webdriver.Firefox(firefox_profile=fp)
+            driver.set_window_size(1024, 768)
+            driver.implicitly_wait(40)
 
-			##SUMMARY TAB
-			#download heavy and light csvs 	
-			driver.find_element_by_link_text("Summary").click()
-			element = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.ID, "summary_table")))
-			driver.find_element_by_link_text("Download Heavy Chain Table").click()
-			driver.find_element_by_link_text("Download Light Chain Table").click()
-			print "Saved csvs to file..."
-			
-			#pdb model (B value replaced with contact probabilities (higher prob = darker colour))
-			driver.find_element_by_link_text("Download 3D Model").click()
-			#sleep 3 seconds to give you time to click "ok"
-			sleep(3)
-			print "Saved PDB model..."
-			
-			print ("Bye!")
+            #get page
+            driver.get("http://circe.med.uniroma1.it/proABC/")
+            element = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "invio")))
+            print "Got page..."
 
-		except Exception, e:
-		    driver.save_screenshot("exception_screen" + datetime.date.today().strftime('%d-%b-%Y') + '.png')
-		    print "Saved exception screenshot."
-		    traceback.print_exc(file=sys.stdout)
+            #fill form
+            driver.find_element_by_name("name").send_keys(hvy_project_names[i])
+            driver.find_element_by_name("email").send_keys("jlaffy7@gmail.com")
+            driver.find_element_by_id("light").send_keys(light_chains[j])
+            driver.find_element_by_id("heavy").send_keys(hvy_chains[i])
+            #driver.find_element_by_xpath("//select[@name='volume']/option[@value='"+vol+"']").click)
+            driver.find_element_by_id("invio").click()
+            print "Filled form..."
 
-		finally:
-			driver.close()
+            #goes to the Plot tab as default
+            element = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.ID, "IDgoogleTable")))
+
+            ##SUMMARY TAB
+            #download heavy and light csvs
+            driver.find_element_by_link_text("Summary").click()
+            element = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.ID, "summary_table")))
+            driver.find_element_by_link_text("Download Heavy Chain Table").click()
+            driver.find_element_by_link_text("Download Light Chain Table").click()
+            print "Saved csvs to file..."
+            os.rename(os.path.join(path, oldheavy), os.path.join(path, newheavy))
+            ##os.rename("~/36Abs/results/rawdat_proabc/"+light_project_names[i]+"/Prob_Light.csv", "~/36Abs/results/rawdat_proabc/"+light_project_names[i]+"/"
+
+            ##pdb model (B value replaced with contact probabilities (higher prob = darker colour))
+            ##driver.find_element_by_link_text("Download 3D Model").click()
+            ##sleep 3 seconds to give you time to click "ok"
+            ##sleep(3)
+            ##print "Saved PDB model..."
+
+            print ("Bye!")
+
+        except Exception, e:
+            driver.save_screenshot("exception_screen" + datetime.date.today().strftime('%d-%b-%Y') + '.png')
+            print "Saved exception screenshot."
+            traceback.print_exc(file=sys.stdout)
+
+        finally:
+            driver.close()
