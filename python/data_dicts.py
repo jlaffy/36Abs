8e3fc7498387da4b1b8aac5ee7139fc7c51f0ad0
Julie Laffy <julielaffy@apomac.local>
2015-08-18 17:44:15 +0100

    made example csv for NAIL120 , antigen volumen not known alignments.

---
+++ python/data_dicts.py
@@ -0,0 +1,22 @@
+# !/usr/bin/python
+
+# import csv
+# import glob
+# import os
+#
+# path = "/Users/julielaffy/36Abs/results/rawdat_proabc/NAIL120/"
+# immh103MV2 = "immh149MV5"
+# new_directory = os.chdir(path)
+# for csv in glob.glob("*"immh103MV2"*Heavy*"):
+#     print csv
+#         # reader = csv.DictReader(csvfile, delimiter = ',')
+#         # for row in reader:
+#         #     print (row[1], row[133])
+#
+
+
+tuplest = 'A', 8, 78.5, '9'
+letter, ascii_score, probability, probability_score = tuplest
+print letter
+
+
